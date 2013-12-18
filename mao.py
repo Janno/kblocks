2aaa1ee5f5b185d313c87eb68235c2ce4b2702d8
Janno <janno@janno-svd13>
2013-12-18 12:19:08 +0000

    class to hold all state of the mao algorithm

--- mao.py
+++ mao.py
@@ -11,6 +11,64 @@ def is_mao(g, m):
                 return False
     return True
 
+class MaoState(object):
+    def __init__(self, g, L=None, V=None, hi=None):
+        if not L:
+            L = defaultdict(list)
+            L[0].extend(graph.nodes_iter()) 
+        if not V:
+            V = {v:(0, i) for i,v in enumerate(L[0])}
+        if not hi:
+            hi = 1
+        self.L = L 
+        self.V = V
+        self.hi = hi
+
+    def copy(self):
+        L_new = defaultdict(list)
+        for k, l in self.L.iteritems():
+            L_new[k] = l[:]
+        return MaoState(g, L_new, dict(self.V.iteritems()), hi)
+
+    def update_hi(self):
+        while not L[self.hi] and self.hi > 0:
+            self.hi = self.hi - 1
+
+    def push(self, u):
+        # TODO this is O(m log m) and this is called O(n) times. fook.
+        # TODO why do I even sort this?
+        for v in sorted(graph[u], key=self.V.get, reverse=True):
+            # print '\tneighbour', v
+            if not v in self.V:
+                continue
+            hp, tp = self.V[v]
+            self.hi = max(self.hi, hp+1)
+            # swap v last element in current list L[hp], if necessary
+            if tp < len(L[hp])-1:
+                v_last = self.L[hp][-1]
+                self.V[v_last] = (hp, tp) 
+                self.L[hp][tp], self.L[hp][-1] = self.L[hp][-1], self.L[hp][tp]
+                tp = len(self.L[hp])-1 
+            # append v to higher list
+            self.V[v] = hp+1, len(self.L[hp+1])
+            self.L[hp+1].append(v)
+            # delete old occurence of v which is now at the last index in L[hp]
+            del self.L[hp][tp]
+
+    def candidates(self):
+        return self.L[self.hi]
+
+    def step(self, i):
+        v = self.L[self.hi][i]
+        del self.L[self.hi][i]
+        del self.V[v]
+
+        self.update_hi()
+
+        # if we are not done we need to update L
+        if L[hi]:
+            hi_new = push(v)
+
 def mao(graph, s):
     m = [s]
     # mimic linked list of linked lists (outer layer is lazy)
